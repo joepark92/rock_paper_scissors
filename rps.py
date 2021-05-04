f1bc687f2b253e5e19db4414a366e7afe4a8f257
Joe Park <joepark92@gmail.com>
2021-05-04 16:05:48 -0700

    no longer case sensitive

--- rps.py
+++ rps.py
@@ -6,13 +6,16 @@ player_wins = 0
 computer_wins = 0
 
 while (computer_wins < 3 or player_wins < 3):
-    rpk_input = input("Enter your weapon: ")
+    rpk_input = input('Enter your weapon: ').lower()
     rpk = ["hammer", "net", "katana"]
     computer_action = random.choice(rpk)
+
     print("----- ROUND START -----")
     print(f"You chose {rpk_input}, computer chose {computer_action}")
+
     if rpk_input == computer_action:
         print(f"Both players selected {rpk_input}. It's a tie!")
+
     elif rpk_input == "hammer":
         if computer_action == "katana":
             print("hammer shatters katana! You win!")
@@ -20,6 +23,7 @@ while (computer_wins < 3 or player_wins < 3):
         else:
             print("net covers hammer! You lose.")
             computer_wins += 1
+
     elif rpk_input == "net":
         if computer_action == "hammer":
             print("net covers hammer! You win!")
@@ -27,6 +31,7 @@ while (computer_wins < 3 or player_wins < 3):
         else:
             print("katana slices net! You lose.")
             computer_wins += 1
+
     elif rpk_input == "katana":
         if computer_action == "net":
             print("katana slices net! You win!")
@@ -34,6 +39,7 @@ while (computer_wins < 3 or player_wins < 3):
         else:
             print("hammer shatters katana! You lose.")
             computer_wins += 1
+            
     else:
         print("Invalid choice. Please enter hammer, net, or katana!")
     
